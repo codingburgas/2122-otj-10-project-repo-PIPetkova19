d782bd6e92a023ee35bbe7e4874f68c59030f5e7
Петя Илиева Петкова <PIPetkova19@codingburgas.bg>
2022-03-16 16:54:13 +0200

    Add structure

--- Project/ConsoleApplication1/ConsoleApplication1.cpp
+++ Project/ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,12 +6,29 @@ using namespace std;
 
 double* foo()
 {
-    double* arr = new double(10);
-    delete[] arr;
-    return arr;
+	double* arr = new double(10);
+	delete[] arr;
+
+	return arr;
 }
+
+struct USER
+{
+	size_t id;
+	string name;
+	string email;
+	uint8_t age;
+};
+
+
 
 int main()
 {
-
+	vector<USER> users
+	{
+	{1, "user1", "user1@gmail.com", 16},
+	{2, "user1", "user2@gmail.com", 17},
+	{3, "user1", "user3@gmail.com", 18},
+	{4, "user1", "user4@gmail.com", 19}
+	};
 }
