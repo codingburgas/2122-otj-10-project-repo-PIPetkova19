6abe52e8c52259e7f15266a85fc01893773c32b4
Петя Илиева Петкова <PIPetkova19@codingburgas.bg>
2022-03-23 17:04:49 +0200

    Add static library

---
+++ Lesson/lesson 23.3/StaticLib1/user.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "pch.h"
+
+namespace pm::types {
+	struct User {
+		size_t id;
+		std::string firstName;
+		std::string lastName;
+		std::string email;
+		uint8_t age;
+		std::string passwordHash;
+	};
+}
